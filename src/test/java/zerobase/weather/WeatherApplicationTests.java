dd5105b4b1204e53d1001c01d72adaaa460921f0
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2022-09-13 12:14:47 +0900

    날씨 일기 프로젝트

---
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -0,0 +1,28 @@
+package zerobase.weather;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void equalTest() {
+		assertEquals(1, 1);
+	}
+
+	@Test
+	void nullTest() {
+		assertNull(null);
+	}
+
+	@Test
+	void trueTest() {
+		assertTrue(1==1);
+	}
+
+}
